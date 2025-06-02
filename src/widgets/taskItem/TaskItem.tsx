b44c4b9eb7436ac3a21de78d1d85cbbd8a59c72e
VitMach <machihin_vitalii@mail.ru>
2025-06-02 17:56:31 +0300

    Добавление Dropdown

--- src/widgets/taskItem/TaskItem.tsx
+++ src/widgets/taskItem/TaskItem.tsx
@@ -1,18 +1,36 @@
 import { FC } from 'react';
 import { type Task } from '@/shared/types/task';
-import { Checkbox } from '@/shared/ui';
+import { Button, Checkbox, Icon } from '@/shared/ui';
 
 import styles from './taskItem.module.css';
+import { useTaskEditorStore, useTaskStore } from '@/app/store/store';
 
 interface ITaskProps {
   task: Task;
 }
 
 export const TaskItem: FC<ITaskProps> = ({ task }) => {
+  // const { toggleTaskEditor } = useTaskEditorStore();
+
+  // const onOpenTaskEditor = useCallback(
+  //   (id) => {
+  //     handleOpenTaskEditor(TYPE_TASK.edit, id);
+  //   },
+  //   [handleOpenTaskEditor]
+  // );
+
   return (
     <li className={styles.item} key={task.id}>
-      <Checkbox {...task} />
-      <span className={styles.title_task}>{task.title}</span>
+      <div className={styles.inner}>
+        {/* <Checkbox task={task} /> */}
+        <span className={styles.titleTask}>{task.title}</span>
+      </div>
+
+      <Button
+        icon={<Icon name='edit' />}
+        className={styles.taskEdit}
+        // onClick={() => onOpenTaskEditor(task.id)}
+      />
     </li>
   );
 };
